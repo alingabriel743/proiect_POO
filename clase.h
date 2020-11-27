b475c1dd2f645e3b9116563f077679b3b7d0d415
Alin Vaduva <35081533+alingabriel743@users.noreply.github.com>
2020-11-27 19:47:01 +0200

    Avem si select acum

--- clase.h
+++ clase.h
@@ -1,83 +1,79 @@
 #pragma once
+#pragma warning(disable:4996)
 #include <iostream>
 #include <string>
 using namespace std;
 
+//enum Comenzi { SELECT = 1, UPDATE = 2, INSERT = 3, DELETE = 4, CREATE = 5, DROP = 6, DISPLAY = 7 };
 
-
-enum Comenzi {SELECT = 1, UPDATE = 2, INSERT = 3, DELETE = 4, CREATE = 5, DROP = 6, DISPLAY = 7};
-
-class Interpretor {
-private:
-	char* comanda = nullptr;
-	int dimensiune;
-	char** restComanda = nullptr;
+class ExceptieComandaGresita {
+	string mesaj = "";
 public:
-	Interpretor() {
-		dimensiune = 0;
+	ExceptieComandaGresita(){}
+	ExceptieComandaGresita(string eroare) {
+		this->mesaj = eroare;
 	}
-	Interpretor(string comandaIntreaga, int size) : dimensiune(size) {
-		char* input = new char[this->dimensiune + 1];
-		int i;
-		for (i = 0; i < this->dimensiune; i++) {
-			input[i] = comandaIntreaga[i];
-		}
-		input[i + 1] = '\0';
-		char* numeComanda = strtok(input, " ");
 
-		this->comanda = numeComanda;
-		int nrCuvinte = 1;
-		for (int j = strlen(this->comanda)+2; j < comandaIntreaga.size(); j++) {
-			if (comandaIntreaga[j] == ' ') nrCuvinte++;
-		}
-		this->restComanda = new char*[nrCuvinte];
-		char* restInput = new char[this->dimensiune - (strlen(this->comanda) + 1)];
-		int k = 0;
-		int cont = 0;
-		for (cont,k = strlen(this->comanda) + 1; k < comandaIntreaga.size(); k++, cont++) {
-			restInput[cont] = comandaIntreaga[k];
-		}
-		restInput[k + 1] = '\0';
-
-		for (int i = 0; i < nrCuvinte; i++) {
-			this->restComanda[i] = strtok(restInput, " ");
-		}
-
-		for (int i = 0; i < nrCuvinte; i++) {
-			for (int j = 0; j < strlen(this->restComanda[i]); j++) {
-				cout << this->restComanda[i][j];
-			}
-			cout << endl;
-		}
-
-		//cout << nrCuvinte;
-		//for (int i = 0; i < strlen(this->comanda); i++) cout << this->comanda[i];
-
+	string getMesaj() {
+		return this->mesaj;
 	}
-	void tipComanda() {
-		if (strcmp(this->comanda, "SELECT")) {
-			//Select select;
-		}
-
-	}
-	friend class Select;
-	friend class Update;
-	friend class Insert;
-	friend class Delete;
-	friend class Create;
-	friend class Drop;
-	friend class Display;
 
 };
 
-
 class Select {
 private:
+	char** parametriIntrare = nullptr;
+	int nrParametriIntrare;
 	char** numeColoane = nullptr;
 	char* numeTabela = nullptr;
-
+	char* numeColoanaVerificata = nullptr;
+	char* valoare = nullptr; //atoi
 public:
-	friend class Interpreter;
+
+	Select(char** parametriIntrare, int nrParam) {
+		this->parametriIntrare = parametriIntrare;
+		this->nrParametriIntrare = nrParam;
+	}
+	void Test() {
+		for (int i = 0; i < nrParametriIntrare; i++)
+			cout << this->parametriIntrare[i] << endl;
+	}
+
+	void filtrareElemente() {
+		int index = 0;
+		while (strcmp(this->parametriIntrare[index + 1], "FROM") != 0) {
+			index++;
+		}
+		this->numeColoane = new char* [index];
+		int nrCol = 0;
+		for (int j = 1; nrCol < index; nrCol++, j++) {
+			this->numeColoane[nrCol] = new char[strlen(this->parametriIntrare[j]) + 1];
+			strcpy(this->numeColoane[nrCol], this->parametriIntrare[j]);
+		}
+
+
+		if (index) {
+			this->numeTabela = new char[strlen(this->parametriIntrare[index + 2])];
+			strcpy(this->numeTabela, this->parametriIntrare[index + 2]);
+		}
+		index += 3;
+		bool existaColoana = false;
+		if (index < this->nrParametriIntrare) {
+			for (int i = 0; i < nrCol; i++) {
+				if (strcmp(this->numeColoane[i], this->parametriIntrare[index + 1]) == 0) {
+					existaColoana = true;
+					break;
+				}
+			}
+		}
+	}
+	// afisam un mesaj pentru a informa in legatura cu utilitatea comenzii
+
+	friend class Interpretor;
+};
+
+class Create {
+
 };
 
 class Update {
@@ -88,13 +84,11 @@ class Insert {
 
 };
 
-class Delete{
+class Delete {
 
 };
 
-class Create {
 
-};
 
 class Drop {
 
@@ -108,6 +102,7 @@ class Tabela {
 private:
 	const int id;
 	static int nrTabela;
+	//vector de tabele pentru a tine minte existenta lor
 public:
 	//ceva
 };
@@ -121,5 +116,81 @@ private:
 	string descriere;
 	static unsigned int nrColoane;
 };
-
 unsigned int Coloana::nrColoane = 0;
+
+
+
+class Interpretor {
+private:
+	char* numeComanda = nullptr;
+	int dimensiuneComanda;
+	char** parametriComanda = nullptr;
+	int nrParametri;
+public:
+	Interpretor() {
+		this->dimensiuneComanda = 0;
+		this->nrParametri = 0;
+	}
+	Interpretor(string comandaIntreaga, int size) : dimensiuneComanda(size) {
+		bool temp = false;
+		int nrCuvinte = 0;
+		for (int j = 0; j < comandaIntreaga.size(); j++) {
+			if (comandaIntreaga[j] == ' ' || comandaIntreaga[j] == '(' || comandaIntreaga[j] == '=')
+				temp = false;
+			else if (!temp) {
+				temp = true;
+				nrCuvinte++;
+			}
+		}
+		this->nrParametri = nrCuvinte;
+		this->parametriComanda = new char* [nrCuvinte];
+		char* input = new char[this->dimensiuneComanda + 1];
+		int lungimeInput = 0;
+		for (lungimeInput; lungimeInput < this->dimensiuneComanda; lungimeInput++) {
+			input[lungimeInput] = comandaIntreaga[lungimeInput];
+		}
+		input[lungimeInput] = '\0';
+		char delim[] = " =,()";
+		char* numec = strtok(input, delim);
+
+		int i = 0;
+		while (numec) {
+			this->parametriComanda[i] = new char[strlen(numec) + 1];
+			strcpy(this->parametriComanda[i], numec);
+			numec = strtok(nullptr, delim);
+			if(numec) i++;
+		}
+		this->numeComanda = this->parametriComanda[0];
+	}
+
+	void initializareComenzi() {
+		if (strcmp(this->numeComanda, "SELECT") == 0) {
+			Select s(this->parametriComanda, this->nrParametri);
+			s.Test();
+			s.filtrareElemente();
+		}
+		else if (strcmp(this->numeComanda, "CREATE") == 0) {
+
+		}
+	}
+
+	~Interpretor() {
+		for (int i = 0; i < this->nrParametri; i++) {
+			delete[] this->parametriComanda[i];
+		}
+		delete[] this->parametriComanda;
+	}
+
+	friend class generareObiecte;
+	friend class Select;
+	
+	// ca sa facem conexiunea folosim si noi un operator
+	friend ostream& operator<<(ostream& consola, Interpretor& inte) {
+		//consola << inte.nrParametri;
+		inte.initializareComenzi();
+		//consola << "Buna ziua";
+		return consola;
+	}
+
+};
+
--- proiect_POO.cpp
+++ proiect_POO.cpp
@@ -7,17 +7,9 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	string comanda;
-	getline(cin, comanda, ';');
-	/*char* input = new char[text.size() + 1];
-	int i;
-	for (i = 0; i < text.size(); i++) {
-		input[i] = text[i];
-	}
-	input[i + 1] = '\0';
-    char* token = strtok(input, " ");
-	for (int i = 0; i < strlen(token); i++) cout << token[i];*/
-
+	getline(cin, comanda);
     //pentru citirea mai multor comenzi folosim un while(exit)
 	Interpretor interp(comanda, comanda.size());
-
+	//interp.initializareComenzi();
+	cout << interp;
 }
