081737661f01e6af0d0e0f1b81b6d325220ae1f7
AnaMTF <74465055+AnaMTF@users.noreply.github.com>
2021-01-19 16:58:59 +0200

    Update proiect_POO.cpp

--- proiect_POO.cpp
+++ proiect_POO.cpp
@@ -6,22 +6,50 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	AccesFisierComenzi acs(argv[1]);
-	//CreareFisier scriere(acs);
-	string* comanda = acs.returnareComanda();
-	for (int i = 0; i < acs.getNrLiniiFisier(); i++) {
-		try {
-			cout << "============================" << endl;
-			Interpretor interp(comanda[i], comanda[i].size());
-			cout << interp;
-			cout << endl;
+	bool tastatura = true; //presupunem ca utilizatorul doreste introducerea comenzilor de la tastatura
+	char decizie;
+	cout << "Bine ati venit! Doriti sa utilizati un fisier de comenzi sau sa utilizati tastatura? Fisier - F / Tastatura - T" << endl;
+	cin >> decizie;
+	if (decizie == 'T') {
+		bool exit = false; //utilizatorul continua sa introduca comenzi
+		char alegere;
+		while (!exit) {
+			cout << "Introduceti comanda: " << endl;
+			cin.ignore(1000, '\n');
+			string comanda;
+			getline(cin, comanda);
+			try {
+				cout << "============================" << endl;
+				Interpretor interp(comanda, comanda.size());
+				cout << interp;
+			}
+			catch (ExceptieComandaGresita& ex) {
+				cout << ex.getMesaj() << endl;
+			}
+			cout << endl << "Continuati sa introduceti comenzi? D/N" << endl;
+			cin >> alegere;
+			if (alegere == 'N') exit = true;
+			else continue;
 		}
-		catch (ExceptieComandaGresita& ex) {
-			cout << ex.getMesaj() << endl;
+	}
+	else if (decizie == 'F') {
+		AccesFisierComenzi acs(argv[1]);
+		//CreareFisier scriere(acs);
+		string* comanda = acs.returnareComanda();
+		for (int i = 0; i < acs.getNrLiniiFisier(); i++) {
+			try {
+				cout << "============================" << endl;
+				Interpretor interp(comanda[i], comanda[i].size());
+				cout << interp;
+				cout << endl;
+			}
+			catch (ExceptieComandaGresita& ex) {
+				cout << ex.getMesaj() << endl;
+			}
 		}
 	}
 
-	/*Users u1("Popescu Andrei", 2), u2("Ana Titeche", 7), u3("Vaduva Alin", 10), u4("John Michaels", 18);
+	Users u1("Popescu Andrei", 2), u2("Ana Titeche", 7), u3("Vaduva Alin", 10), u4("John Michaels", 18);
 	vector<Users> us(10);
 	us.reserve(20);
 	cout << "\nDimensiunea vectorului: " << us.size() << " cu extindere pana la " << us.capacity() << " elemente" << endl;
@@ -34,9 +62,9 @@ int main(int argc, char* argv[]) {
 	for (i = us.begin(); i != us.end(); i++) {
 		cout << *i << " ";
 	}
-	cout << endl;*/
+	cout << endl;
 
-	/*list<Users> li = { u1, u2, u3, u4 };
+	list<Users> li = { u1, u2, u3, u4 };
 	list<Users>::iterator it;
 	Users u5("Silviu", 777);
 	li.push_back(u5);
@@ -44,7 +72,7 @@ int main(int argc, char* argv[]) {
 		cout << *it << " ";
 	}
 
-	set<Users> s1 = { u1, u2, u3, u4, u5 };
+	/*set<Users> s1 = { u1, u2, u3, u4, u5 };
 	set<Users>::iterator its;
 	for (its = s1.begin(); its != s1.end(); its++) {
 		cout << &its << ", ";
